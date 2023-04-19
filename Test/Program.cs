f76b6a71784456d92ca6b77180061d01b348f763
Tokiniaina <randrianarimananatokiniaina47@gmail.com>
2023-04-19 11:00:29 +0300

    Ajoutez des fichiers projet.

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person { Name = "toki" };
+            Console.WriteLine($"Hello {person.Name}");
+            Console.ReadLine();
+        }
+    }
+}
